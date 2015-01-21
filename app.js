34f35c1bc0ea56c23f98419bee2468cbf7f615b7
Cillianbc <cillianbc@gmail.com>
2015-01-21 21:30:50 +1100

    Commit

--- app.js
+++ app.js
@@ -2,11 +2,11 @@
 function fizzBuzz(entry){
   for (i=1; i <= entry; i++){
     if (i % 3 === 0)
-      $('body').append("<li>Fizz</li>").text();
+      $('body ul').append("<li>Fizz</li>").text();
     else if (i % 5 === 0)
-      $('body').append("<li>Fizz</li>").text();
+      $('body ul').append("<li>Fizz</li>").text();
     else 
-      $('body').append("<li>"+i+"</li>").text();
+      $('body ul').append("<li>"+i+"</li>").text();
 }};
 var entry = prompt('Enter a number between 1 & 100');
 fizzBuzz(entry);

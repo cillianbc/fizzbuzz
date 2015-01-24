338c637f332e794b55413482e8373a70bd58d637
Cillianbc <cillianbc@gmail.com>
2015-01-24 13:58:16 +1100

    Removed .text()

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var entry = prompt('Enter a number between 1 & 100');
 function fizzBuzz(entry){
   for (i=1; i <= entry; i++){
     if (i % 3 === 0)
-      $('body').append("<li>Fizz</li>").text();
+      $('body').append("<li>Fizz</li>");
     else if (i % 5 === 0)
-      $('body').append("<li>Buzz</li>").text();
+      $('body').append("<li>Buzz</li>");
     else 
-      $('body').append("<li>"+i+"</li>").text();
+      $('body').append("<li>"+i+"</li>");
 
 }}
 fizzBuzz(entry);

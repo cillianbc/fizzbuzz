0eff6c4157d39bd270cde2988a3564fd641647c4
Cillianbc <cillianbc@gmail.com>
2015-02-10 21:37:35 +1100

    It works, number check completed

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-var entry = prompt('Enter a number between 1 & 100');
-function fizzBuzz(entry){
-  for (i=1; i <= entry; i++){
-    if (i % 3 === 0)
-      $('body').append("<li>Fizz</li>").text();
-    else if (i % 5 === 0)
-      $('body').append("<li>Fizz</li>").text();
-    else 
-      $('body').append("<li>"+i+"</li>").text();
-
-}}
-fizzBuzz(entry);
-
+function fizzBuzz() {
+  var entry = prompt('Enter a number between 1 & 100');
+  if (isNaN(entry)){
+        alert("Please enter a number")
+        fizzBuzz();
+  }
+  else {
+    for (i = 1; i <= entry; i++){
+      if (i % 3 === 0 && i % 5===0)
+        $('body').append("<li>FizzBuzz</li>");
+      else if (i % 3 ===0)
+        $('body').append("<li>Fizz</li>");
+      else if (i % 5 === 0)
+        $('body').append("<li>Buzz</li>");
+      else
+        $('body').append("<li>"+i+"</li>");
+    }
+  }
+}
+fizzBuzz();
